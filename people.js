437a7e1a39f97ffac93a0896f677119db6d44d79
FumbblWreckage <tusel@gmx.de>
2020-03-21 04:26:28 +0100

    Add files via upload

---
+++ people.js
@@ -0,0 +1,13 @@
+const followers = {
+    window: false
+}
+document.getElementById("followers").addEventListener("click", () => {
+    if (followers.window) {
+
+    } else {
+
+
+        followers.window = true;
+    }
+
+})
