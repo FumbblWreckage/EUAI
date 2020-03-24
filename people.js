54c52e56ae4d9b847f9d44a84dca48f758f6a41c
FumbblWreckage <tusel@gmx.de>
2020-03-24 18:41:45 +0100

    Update people.js

--- people.js
+++ people.js
@@ -2,11 +2,12 @@ const followers = {
     window: false
 }
 document.getElementById("followers").addEventListener("click", () => {
+  const peopleList =  document.getElementById("peopleList"); 
     if (followers.window) {
-
+peopleList.style.display = "block";
     } else {
 
-
+peopleList.style.display = "none";
         followers.window = true;
     }
 
