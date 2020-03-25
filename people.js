dd30c30495fd1cac5897feae5a76db8b365718c4
FumbblWreckage <tusel@gmx.de>
2020-03-25 21:12:33 +0100

    Update people.js

--- people.js
+++ people.js
@@ -5,17 +5,17 @@ const followers = {
 }
 
 const persons = [
-name: "Dr. Nisaei",
-profession: "Scientist",
-followers: 20,
-influence: 10,
-skill: 70,
+    name: "Dr. Nisaei",
+    profession: "Scientist",
+    followers: 20,
+    influence: 10,
+    skill: 70,
     attaintiveness: 30,
     cost: 50000
 
 ]
 
-
+addPersons(persons[0])
 document.getElementById("followers").addEventListener("click", () => {
  var peopleList =  document.getElementById("peopleList"); 
     if (followers.window) {
@@ -32,8 +32,14 @@ peopleList.style.display = "block";
 
 }
 
-function addFollower(){
+function addFollower(follower){
 var row = document.getElementById("peopleStats").appendChild(document.createElement("tr"))
-row.innerHTML = 
+row.innerHTML = "<tr><td>"+follower.name+"</td><td>"+follower.profession+\
+    "</td><td>"+follower.followers+\
+    "</td><td>"+follower.influence+\
+    "</td><td>"+follower.skill+\
+    "</td><td>"+follower.attaintiveness+\
+    "</td><td>"+follower.cost+"</td></tr>"
+    
 }
 
