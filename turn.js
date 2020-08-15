580c5ebab3d1d05c4d75f9f73af2873bdcb2a81d
FumbblWreckage <tusel@gmx.de>
2020-08-16 00:33:33 +0200

    Add files via upload

---
+++ turn.js
@@ -0,0 +1,48 @@
+var eventsInGermany = events: {
+	GermanyTurn1: {
+	where: "Germany",
+	countryID: "de",
+	turn: 1,
+	person: "Nicole Wagenknecht",
+	personNo: 1,
+	description: "Convince left wing leader Nicole Wagenknecht to join your cause."}
+}
+
+
+
+
+
+function resolveTurn(turn, country){
+
+
+	
+	
+	
+addPeople(turn, country);
+
+		
+}
+
+
+
+function addPeople(numberOrName, country){
+
+
+if numberOfName === !isNAN(numberOrName){
+var number = numberOrName;	
+//add condition for when it is a name later	
+}
+let newActionObject = {
+	name: eventsInGermany.description,
+	type: function()=>addPerson(number=number),
+	operator: "remove"
+	
+}
+
+
+actionList[country].push(newActionObject);
+	
+	
+}
+
+
